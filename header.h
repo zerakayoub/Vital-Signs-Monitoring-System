f0ad813121a653321e9737e38c9a765c990650ad
Zerak Ayoub <Zerak.ayoub@gmail.com>
2026-03-10 02:43:12 -0700

    create main files

---
+++ header.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <vector>
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+#include "header.h"
 
 int main()
 {
